496072c8103b6b9e600ea757cef94c59e3c5deec
Voronovich_dev <alexanderbizv1@gmail.com>
2022-10-14 17:48:55 +0300

    feat: create mockup for FullPizza component

---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+const FullPizza = () => {
+  const [pizza, setPizza] = useState();
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get(`https://633eacc90dbc3309f3ba904c.mockapi.io/items/${id}`);
+        setPizza(data);
+      } catch (error) {
+        alert('Error');
+      }
+    }
+    fetchPizza();
+  }, []);
+
+  if (!pizza) {
+    return <p>loading...</p>;
+  }
+
+  return (
+    <div className="container">
+      <img src={pizza.imageUrl} alt={pizza.title} />
+      <h2>{pizza.title}</h2>
+      <h4>{pizza.price} $</h4>
+    </div>
+  );
+};
+
+export default FullPizza;
