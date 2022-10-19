8f8dd23756376eed7e89576d0310b08b24bab21b
Voronovich_dev <alexanderbizv1@gmail.com>
2022-10-19 17:54:40 +0300

    refactor: dinamically import and lazy load Cart-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 import FullPizza from './pages/FullPizza';
 
 import './scss/app.scss';
 import MainLayout from './layouts/MainLayout';
 
+const Cart = React.lazy(() => import('./pages/Cart'));
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route
+          path="/cart"
+          element={
+            <Suspense fallback={<div>Cart is loading...</div>}>
+              <Cart />
+            </Suspense>
+          }
+        />
         <Route path="/pizza/:id" element={<FullPizza />} />
         <Route path="*" element={<NotFound />} />
       </Route>
