74dd09b5315220955632e2d11739142a6570541e
Voronovich_dev <alexanderbizv1@gmail.com>
2022-10-19 18:12:13 +0300

    fix: add GoBack-btn at FullPizza component

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 
 const FullPizza: React.FC = () => {
   const [pizza, setPizza] = useState<{
@@ -34,6 +34,27 @@ const FullPizza: React.FC = () => {
       <h2>{pizza.title}</h2>
       <h4>{pizza.price} $</h4>
       <img src={pizza.imageUrl} alt={pizza.title} />
+      <div className="cart__bottom-buttons">
+        <Link to="/" className="button button--outline button--add go-back-btn">
+          <svg
+            width="8"
+            height="14"
+            viewBox="0 0 8 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7 13L1 6.93015L6.86175 1"
+              stroke="#D3D3D3"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+
+          <span>Go back</span>
+        </Link>
+      </div>
     </div>
   );
 };
